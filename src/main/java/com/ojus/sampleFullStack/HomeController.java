019f9952471a5a714219efe0cf4b281a3e2a7424
Ojus Singhal <ojus.singhal17@gmail.com>
2021-06-09 21:41:47 +0530

    SDFA

--- src/main/java/com/ojus/sampleFullStack/HomeController.java
+++ src/main/java/com/ojus/sampleFullStack/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController
 {
     @RequestMapping()
