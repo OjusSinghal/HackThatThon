9546fa218f037962c9980cf931da963c15b6d8a4
Ojus Singhal <ojus.singhal17@gmail.com>
2021-06-10 00:20:53 +0530

    sdf

--- src/main/java/com/ojus/sampleFullStack/PersonResource.java
+++ src/main/java/com/ojus/sampleFullStack/PersonResource.java
@@ -6,12 +6,14 @@ import com.ojus.sampleFullStack.repo.PersonRepo;
 import com.ojus.sampleFullStack.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping()
 public class PersonResource
 {
@@ -28,45 +30,46 @@ public class PersonResource
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index.html");
 
-        System.out.println("home page through model and view");
+        System.out.println(" uff home page through model and view");
         return mv;
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Person>> getAllPerson()
+    public @ResponseBody ResponseEntity<List<Person>> getAllPerson()
     {
         return new ResponseEntity<>(personService.findAllPersons(), HttpStatus.OK);
     }
 
     @GetMapping("/find/{id}")
-    public ResponseEntity<Person> findPersonById(@PathVariable Long id)
+    public @ResponseBody ResponseEntity<Person> findPersonById(@PathVariable Long id)
     {
         return new ResponseEntity<>(personService.findPerson(id), HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Person> addPerson(@RequestBody Person person)
+    public @ResponseBody ResponseEntity<Person> addPerson(@RequestBody Person person)
     {
         Person newPerson = personService.addPerson(person);
         return new ResponseEntity<>(newPerson, HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<Person> updatePerson(@RequestBody Person person)
+    public @ResponseBody ResponseEntity<Person> updatePerson(@RequestBody Person person)
     {
         Person updatedPerson = personService.updatePerson(person);
         return new ResponseEntity<>(updatedPerson, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deletePersonById(@PathVariable Long id)
+    public @ResponseBody ResponseEntity<?> deletePersonById(@PathVariable Long id)
     {
         personService.deletePerson(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    
     @DeleteMapping("/deleteAll")
-    public ResponseEntity<?> clearDatabase()
+    public @ResponseBody ResponseEntity<?> clearDatabase()
     {
         personService.deleteAllPersons();
         return new ResponseEntity<>(HttpStatus.OK);
